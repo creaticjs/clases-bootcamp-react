5d9fff548ae92c07cbf1c54b97d743deace948e7
“carlosmolano” <carlosantoniomolanosolarte@gmail.com>
2019-04-12 19:55:38 -0500

    Webpack introduccion

---
+++ webpack/index.js
@@ -0,0 +1,7 @@
+var oper = require("./operaciones");
+var $ = require("jquery");
+var linq = require("linq");
+
+var datos = linq.range(1, 10).toArray();
+
+console.log(datos);
---
+++ webpack/operaciones.js
@@ -0,0 +1,14 @@
+module.exports = {
+  suma: function(a, b) {
+    return a + b;
+  },
+  resta: function(a, b) {
+    return a - b;
+  },
+  mul: function(a, b) {
+    return a * b;
+  },
+  div: function(a, b) {
+    return a / b;
+  }
+};
